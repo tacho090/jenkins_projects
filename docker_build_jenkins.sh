63c31f7d22a0caf92776e3759c20957e8f8b870b
Pablo Mora <panastacio@dotdashmdp.com>
2025-02-02 12:54:05 -0600

    add remove parameter to docker startup command

--- docker_build_jenkins.sh
+++ docker_build_jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 2119:8080 -p 50000:50000 -v /home/pablo/jenkins-projects/:/var/jenkins_home --name jenkins-master jenkins/jenkins:lts
+docker run --rm -p 2119:8080 -p 50000:50000 -v /home/pablo/jenkins-projects/:/var/jenkins_home --name jenkins-master jenkins/jenkins:lts
